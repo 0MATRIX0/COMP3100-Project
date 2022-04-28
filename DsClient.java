273c2e5fa27cb311618e9e1d67e280f97de9ecf8
0MATRIX0 <bellamsujith4444666@gmail.com>
2022-04-28 13:06:10 +1000

    The client now uses the most capable server rather than the biggest server

--- DsClient.java
+++ DsClient.java
@@ -16,12 +16,8 @@ public class DsClient {
 			String username = System.getProperty("user.name");
 			// Empty string to store the input stream
 			String str = "";
-			// String arraylist to store dynamically store the big servers (saves memory)
-			List<String> bigServers = new ArrayList<>();
-			// Variable to store the current position of the server instance
-			int currentServer = 0;
-			// boolean variable used to only store the servers once
-			boolean getServers = true;
+
+			int jobID = 0, jobCores = 0, jobMem = 0, jobDisk = 0;
 
 			dos.write(("HELO\n").getBytes());
 			str = (String) in.readLine();
@@ -43,10 +39,19 @@ public class DsClient {
 				if (!(str.equals("NONE"))) {
 					// job variable will store the current job
 					String job = str;
-					// jobID variable will store the ID of the current job
-					int jobID = Integer.parseInt(str.split(" ")[2]);
-
-					dos.write(("GETS All\n").getBytes());
+					if(job.split(" ")[0].equals("JOBN")){
+						// jobID variable will store the ID of the current job
+						jobID = Integer.parseInt(str.split(" ")[2]);
+						// jobCores variable will store the cored required for the job
+						jobCores = Integer.parseInt(str.split(" ")[4]);
+						// jobMem variable will store the memory required for the current job
+						jobMem = Integer.parseInt(str.split(" ")[5]);
+						// jobDisk variable will store the disk space required for the current job
+						jobDisk = Integer.parseInt(str.split(" ")[6]);
+					}
+					System.out.println(jobID + "  " + jobCores + "  " + jobMem + "  " + jobDisk);
+					
+					dos.write(("GETS Capable "+jobCores+" "+jobMem+" "+jobDisk+"\n").getBytes());
 					str = (String) in.readLine();
 					System.out.println(str);	
 
@@ -63,53 +68,19 @@ public class DsClient {
 						System.out.println(str);
 						servers[i] = str;
 					}
-
-					// This if condition will only run once after the program is run. 
-					if(getServers){
-						// maxCores variable will store the maximum numbers of cores available
-						int maxCores = 0;
-						// serverName variable will store the name of the server will maximum cores
-						String serverName = "";
-
-						// getting the max cores value and server name
-						for (int i = 0; i<servers.length; i++) {
-							if(Integer.parseInt(servers[i].split(" ")[4]) > maxCores) {
-								maxCores = Integer.parseInt(servers[i].split(" ")[4]);
-								serverName = servers[i].split(" ")[0];
-							}
-						}
-
-						// adding the server to bidServers arraylist if its cores are same as maxCores
-						for(int i = 0; i<servers.length; i++) {
-							if(servers[i].split(" ")[0].equals(serverName) && Integer.parseInt(servers[i].split(" ")[4]) == maxCores) {
-								bigServers.add(servers[i]);
-							}
-						}
-
-						//setting getServers to false so that the if conditional block will only run once
-						getServers = false;
-					}
 					
-					// bigServer string array will contain the data of a single big server
-					String[] bigServer = bigServers.get(currentServer).split(" ");
-					
+					// capableServer string array will contain the data of a single capable server
+					String[] capableServer = servers[0].split(" ");
+
 					dos.write(("OK\n").getBytes());
 					str = (String) in.readLine();
 					System.out.println(str);
 
 					// checking if the job type is JOBN or not so that jobs are only scheduled when needed
 					if (str.equals(".") && job.split(" ")[0].equals("JOBN")) {
-						// adding 1 to current server so that the next big server will be used
-						currentServer++;
-
-						dos.write(("SCHD " + jobID + " " + bigServer[0] + " " + bigServer[1] + "\n").getBytes());
+						dos.write(("SCHD " + jobID + " " + capableServer[0] + " " + capableServer[1] + "\n").getBytes());
 						str = (String) in.readLine();
 						System.out.println(str);
-
-						// once all the big servers are used, currentServer will reset back to 0 to schedule jobs from the first server again
-						if(currentServer == bigServers.size()) {
-							currentServer = 0;
-						}
 					}
 				} else {
 					dos.write("QUIT\n".getBytes());
